af270dd6c4129019b33a247b8723ba00468bf0ca
MegaJohnny <megajohnny@hotmail.co.uk>
2013-08-27 21:04:37 +0100

    Added state diagram, test code for red deck

--- com/megajohnny/a2a/GameServer.java
+++ com/megajohnny/a2a/GameServer.java
@@ -31,6 +31,7 @@ public class GameServer {
         }
         
         Card[] greenDeck = null;
+        Card[] redDeck = null;
         try {
             greenDeck = readDeck(args[0], CardColour.GREEN);
             for (Card c : greenDeck) {
@@ -44,6 +45,21 @@ public class GameServer {
         }
         catch (IndexOutOfBoundsException e) {
             System.out.println("Malformed green deck file");
+        }
+        
+        try {
+            redDeck = readDeck(args[0], CardColour.RED);
+            for (Card c : greenDeck) {
+                System.out.println("Red card");
+                System.out.println("- Name: "+c.getName());
+                System.out.println("- Desc: "+c.getDesc());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Problem reading the red deck file");
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("Malformed red deck file");
         }
     }
 }
